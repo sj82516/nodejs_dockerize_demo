6f33a5563597f248115a7c488ca6077b309fb553
YuanChieh,Cheng <sj82516@gmail.com>
2019-03-05 08:14:31 +0800

    feat: add docker-compose

--- server.js
+++ server.js
@@ -1,14 +1,38 @@
 const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const Redis = require('ioredis');
+var redis = new Redis({
+  port: 6379,          // Redis port
+  host: 'redis',   // Redis host
+  db: 0
+});
 
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
-// App
-const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello world, this is version 2\n');
-});
+// Connect using MongoClient
+MongoClient.connect("mongodb://mongodb:27017", function(err, client) {
+  if(err){
+    return console.error(err, client);
+  }
+  // Use the admin database for the operation
+  const logCollection = client.db("test").collection("log");
+  // List all the available databases
+  
+  // App
+  const app = express();
+  app.get('/', async (req, res) => {
+    const logList =  await logCollection.find().toArray();
+    const view = await redis.get("view");
+    console.log(logList.length);
+    res.send(`Hello world, this page is viewed ${view} times.\n`);
+    await redis.incr("view");
+    await logCollection.insertOne({
+      log: view
+    });
+  });
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+  app.listen(PORT, HOST);
+  console.log(`Running on version3 http://${HOST}:${PORT}`);
+});
